39e35a5851df9db82730ed1db8547d9957b8ea85
Kiran Challa <kichalla@microsoft.com>
2016-02-22 10:21:12 -0800

    Return void

--- src/Microsoft.AspNetCore.StaticFiles/DirectoryBrowserServiceExtensions.cs
+++ src/Microsoft.AspNetCore.StaticFiles/DirectoryBrowserServiceExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.AspNetCore.Builder;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -15,41 +14,14 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Adds directory browser middleware services.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        public static IServiceCollection AddDirectoryBrowser(this IServiceCollection services)
+        public static void AddDirectoryBrowser(this IServiceCollection services)
         {
             if (services == null)
             {
                 throw new ArgumentNullException(nameof(services));
             }
 
-            return services.AddWebEncoders();
-        }
-
-        /// <summary>
-        /// Adds directory browser middleware services.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
-        /// <param name="configureOptions">
-        /// An action delegate to configure the provided <see cref="DirectoryBrowserOptions"/>.
-        /// </param>
-        /// <returns>A reference to this instance after the operation has completed.</returns>
-        public static IServiceCollection AddDirectoryBrowser(
-            this IServiceCollection services,
-            Action<DirectoryBrowserOptions> configureOptions)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            if (configureOptions == null)
-            {
-                throw new ArgumentNullException(nameof(configureOptions));
-            }
-
-            services.Configure(configureOptions);
-            return services.AddDirectoryBrowser();
+            services.AddWebEncoders();
         }
     }
 }
